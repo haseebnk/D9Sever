a795925e019b7d236c89c6606c70032013028336
unknown <haseebnk37@gmail.com>
2023-02-24 17:23:12 +0500

    C

--- server.js
+++ server.js
@@ -1,36 +1,36 @@
-// const path = require('path');
-// const express = require('express');
-// const app = express();
-// const http = require('http');
-// const server = http.createServer(app);
-// const socketio = require('socket.io');
+const path = require('path');
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const socketio = require('socket.io');
 
-// const io = socketio(server);
-// app.get('/', function (req, res) {
-//   res.write(`<h1>Hello socket</h1> ${process.env.PORT}`);
-//   res.end;
-// });
-// io.on('connection', client => {
-//   console.log(`âš¡: ${client.id} user just connected!`);
-//   client.on('send message', (data) => {
-//     console.log(data);
-//     io.emit('receive message', data);
-//     // client.broadcast.to(message.recieverId).emit( 'message',`${obj}`);
-//   });
-//   client.on( 'new_notification', function(data) {
-//     console.log(data.message);
-//     io.sockets.emit( 'show_notification', { 
-//       message: data 
-//     });
-//   });
+const io = socketio(server);
+app.get('/', function (req, res) {
+  res.write(`<h1>Hello socket</h1> ${process.env.PORT}`);
+  res.end;
+});
+io.on('connection', client => {
+  console.log(`âš¡: ${client.id} user just connected!`);
+  client.on('send message', (data) => {
+    console.log(data);
+    io.emit('receive message', data);
+    // client.broadcast.to(message.recieverId).emit( 'message',`${obj}`);
+  });
+  client.on( 'new_notification', function(data) {
+    console.log(data.message);
+    io.sockets.emit( 'show_notification', { 
+      message: data 
+    });
+  });
 
-//   client.on('disconnect', () => {
-//     console.log('user disconnected');
-//   });
+  client.on('disconnect', () => {
+    console.log('user disconnected');
+  });
   
-// });
+});
 
 
-// const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080;
 
-// server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
